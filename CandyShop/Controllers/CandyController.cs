b830da508dbfaba37f764ef21c950aa4eb9be51f
L. M <a.f.girl89@hotmail.com>
2020-07-02 22:08:29 +0300

    Add project files.

---
+++ CandyShop/Controllers/CandyController.cs
@@ -0,0 +1,40 @@
+﻿using CandyShop.Models;
+using CandyShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Controllers
+{
+    public class CandyController: Controller 
+    {
+        private readonly ICandyRepository _candyRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CandyController(ICandyRepository candyRepository, ICategoryRepository categoryRepository)
+        {
+            _candyRepository = candyRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public IActionResult List()
+
+        {
+            // ViewBag.CurrentCategory = "Bestsellers";
+            //return View(_candyRepository.GetAllCandy);
+            var candyListViewModel = new CandyListViewModel();
+            candyListViewModel.Candies = _candyRepository.GetAllCandy;
+            candyListViewModel.CurrentCategory = "Bestsellers";
+            return View(candyListViewModel);
+        }
+        public IActionResult Details(int id)
+        {
+            var candy = _candyRepository.GetCandyById(id);
+            if (candy == null)
+                return NotFound();
+
+            return View(candy);
+        }
+    }
+}
